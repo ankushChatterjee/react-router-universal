5785a1044cf0635a491eb7498e2bf148e553fb96
Ankush Chatterjee <ac.ankush15@gmail.com>
2018-05-10 11:55:32 +0530

    export from index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import {Router,Switch,Link,Route} from './Router';
+import {Router,Switch,Link,Route,withRouter} from './Router';
 exports.Router = Router;
 exports.Switch = Switch;
 exports.Link = Link;
 exports.Route = Route;
+exports.withRouter = withRouter;
+
