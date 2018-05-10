3ba96f14022e476f236c1c8263ccb6fc9ed28af7
Ankush Chatterjee <ac.ankush15@gmail.com>
2018-05-10 11:54:45 +0530

    Added withRouter

--- Router.js
+++ Router.js
@@ -1,5 +1,7 @@
 import {NativeRouter as Router,Switch,Link,Route} from 'react-router-native';
+import {withRouter} from 'react-router';
 exports.Router = Router;
 exports.Route = Route;
 exports.Link = Link;
 exports.Switch = Switch; 
+exports.withRouter = withRouter;
--- Router.web.js
+++ Router.web.js
@@ -1,5 +1,7 @@
 import {BrowserRouter as Router,Switch,Link,Route} from 'react-router-dom';
+import {withRouter} from 'react-router';
 exports.Router = Router;
 exports.Route = Route;
 exports.Link = Link;
 exports.Switch = Switch; 
+exports.withRouter = withRouter;
